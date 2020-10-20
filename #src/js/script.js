10b342967da5f8cb3f836ec883394f54ee1434ed
Eugen-www <65528143+Eugen-www@users.noreply.github.com>
2020-10-21 02:05:45 +0300

    add to form id. Make a form validation(lite version)

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,39 @@
+// Classes
+class Form{
+	constructor($selector) {
+		this.$el = document.querySelector($selector);
+	}
+	checkFullness() {
+		const inputs = this.$el.querySelectorAll("input");
+		console.log(inputs[0].value.length);
+		inputs.forEach((input, index) => {
+			if (!(inputs[index].value.length > 0)) {
+				input.classList.add("wrong");
+			}
+		})
+	}	
+	checkAuthor() {
+		const authorInput = this.$el.querySelector("#author-input");
+
+		return [...authorInput.value].some(letter => !isNaN(letter));
+	}
+	checkISBN() {
+		const ISBNInput = this.$el.querySelector("#isbn-input");
+
+		return ISBNInput.value > 0;
+	}
+	checkRating() {
+		const ratingInput = this.$el.querySelector("#rating-input");
+		if ([...ratingInput.value].some(letter => isNaN(letter))) {
+			throw new Error("Введите число")
+		} else {
+			return ratingInput.value > 0 && ratingInput.value <= 5;
+		}
+	}
+}
+
 const $mainSection = document.querySelector(".main-section");
+const form = new Form("#form");
 
 $mainSection.addEventListener("mousemove", parallax);
 
